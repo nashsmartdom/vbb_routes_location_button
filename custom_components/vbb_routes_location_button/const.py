7d70869488a12e2cd8076e7b2e09cfd8e14458a4
nashsmartdom <90872280+nashsmartdom@users.noreply.github.com>
2026-05-03 19:35:53 +0200

    Add constants

---
+++ custom_components/vbb_routes_location_button/const.py
@@ -0,0 +1,20 @@
+DOMAIN = "vbb_routes_location_button"
+
+CONF_ORIGIN_ENTITY = "origin_entity"
+CONF_DESTINATION_ID = "destination_id"
+CONF_DESTINATION_NAME = "destination_name"
+CONF_MIN_DEPART_OFFSET_MIN = "min_depart_offset_min"
+CONF_MAX_TRANSFERS = "max_transfers"
+CONF_RESULTS = "results"
+CONF_TOP_N = "top_n"
+
+DEFAULT_NAME = "VBB Alisa Location"
+DEFAULT_ORIGIN_ENTITY = "device_tracker.alisa_phone"
+DEFAULT_DESTINATION_ID = "900130002"
+DEFAULT_DESTINATION_NAME = "S+U Pankow"
+DEFAULT_MIN_DEPART_OFFSET_MIN = 7
+DEFAULT_MAX_TRANSFERS = 1
+DEFAULT_RESULTS = 20
+DEFAULT_TOP_N = 3
+
+VBB_JOURNEYS_URL = "https://v6.vbb.transport.rest/journeys"
